5f233ebbfeecc16cf8dce9ff858424eb763e4291
Soluna <julia3.williamson@gmail.com>
2025-01-26 00:17:18 -0600

    Temporary Merge with Celestial

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -1,14 +1,6 @@
 use bevy::math::DVec3;
-
-pub trait ToPolar {
-    fn polar(self) -> Polar;
-}
-
-impl ToPolar for DVec3 {
-    fn polar(self) -> Polar {
-        return Polar::new(-self.x, -self.y, -self.z);
-    }
-}
+use num::complex::*;
+use std::f64::consts::TAU;
 
 pub struct Polar {
     pub longitude: f64,
@@ -26,12 +18,56 @@ impl Polar {
     }
 }
 
-pub trait ToCartesian {
-    fn cartesian(self) -> DVec3;
+pub struct Horizontal {
+    pub azimuth: f64,
+    pub elevation: f64,
 }
 
-impl ToCartesian for Polar {
-    fn cartesian(self) -> DVec3 {
-        return DVec3::new(-self.longitude, -self.altitude, -self.latitude);
+pub trait CartesianMethods {
+    fn update(&self, radius: f64) -> Self;
+    fn polar(&self) -> Polar;
+}
+
+fn mode(n: f64) -> f64 {
+    (n - 1.0) % 2.0 - 1.0
+}
+
+fn tri(n: f64, r: f64) -> f64 {
+    4.0 * (((n - r) / 4.0) % r - r / 2.0).abs() - r.abs()
+}
+
+fn flip(x: f64, y: f64, r: f64) -> f64 {
+    2.0 * mode(((x + r) / r).floor() + ((y + r) / r).floor()) + 1.0
+}
+
+impl CartesianMethods for DVec3 {
+    fn update(&self, radius: f64) -> Self {
+        let f = flip(self.x, self.y, radius);
+        let x = f * tri(self.x, radius);
+        let y = f * self.y;
+        let z = f * tri(self.z, radius);
+        return DVec3::new(x, y, z);
+    }
+    fn polar(&self) -> Polar {
+        return Polar::new(-self.x, -self.y, -self.z);
+    }
+}
+
+pub trait PolarMethods {
+    fn cartesian(&self) -> DVec3;
+}
+
+impl PolarMethods for Polar {
+    fn cartesian(&self) -> DVec3 {
+        let longitude = self.longitude.to_radians();
+        let latitude = self.latitude.to_radians();
+        let chi: f64 = (2.0 * (longitude + TAU / 2.0)).cos();
+        let rho: f64 = ((2.0 * latitude + TAU / 2.0) / 4.0).tan();
+        let a: f64 =
+            2.0 * rho.powi(2) - ((1.0 + rho.powi(4)).powi(2) - (2.0 * rho * chi).powi(2)).sqrt();
+        let b: f64 = 1.0 + 2.0 * chi * rho.powi(2) - rho.powi(4);
+        let x = Complex64::new(a / b, 0.0).acos();
+        let z = Complex64::new(b / a, 0.0).acos();
+        return DVec3::new(x.re(), -self.altitude, z.im());
     }
 }
